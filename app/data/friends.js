93a9f4835f386065331a2a95f853a2ee8c6db72d
Terence Ro <terencero@Terences-MacBook-Air.local>
2017-01-21 01:25:31 -0600

    add api routes; create newfriend object

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 var friendsArray = [
 	{
-		name: '',
-		photo: '',
-		scores: []
+		name: 'Terence',
+		photo: 'https://mi-od-live-s.legocdn.com/r/www/r/catalogs/-/media/catalogs/characters/dc/mugshots/mugshot%202016/76061_1to1_mf_batman_336.png?l.r2=-798905063',
+		scores: [1, 2,3,4,5,6,7,8,9,10]
 	}
 ];
 
