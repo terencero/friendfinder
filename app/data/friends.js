cd690072893bd70a2cf7a2340225d8ed95a77583
Terence Ro <terencero@Terences-MacBook-Air.local>
2017-01-22 14:42:36 -0600

    data update

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var friendsArray = [
 	{
 		name: 'Terence',
 		photo: 'https://mi-od-live-s.legocdn.com/r/www/r/catalogs/-/media/catalogs/characters/dc/mugshots/mugshot%202016/76061_1to1_mf_batman_336.png?l.r2=-798905063',
-		scores: [1, 2,3,4,5,6,7,8,9,10]
+		scores: [5, 5,5,4,5,3,5,2,3,5]
 	}
 ];
 
